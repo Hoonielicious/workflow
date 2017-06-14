ce13fd22794437c32f17fbad6e33925784aee11f
Hoonie Sun <myeonghoon18@gmail.com>
2017-06-14 11:31:40 +1000

    Added gulp-compass package and deleted bower-components folder

--- gulpfile.js
+++ gulpfile.js
@@ -25,7 +25,16 @@ var gulp = require('gulp'),
 
 		Description: Bundle modules with BrowserifyJS
 	*/
-	browserify = require('gulp-browserify');
+	browserify = require('gulp-browserify'),
+
+	/*
+		Add a new package called gulp-compass to compile a Sass file
+
+		https://www.npmjs.com/package/gulp-compass
+
+		Description: Compile Sass to CSS using Compass
+	*/
+	compass = require('gulp-compass');
 
 	/*
 		Create an array that includes all the javascript files 
@@ -34,6 +43,8 @@ var jsSources = [
 	'components/scripts/test1.js',
 	'components/scripts/test2.js'
 ];
+
+var sassSources = ['components/sass/styles.scss'];
 
 // Log something during task with gulp-util package
 gulp.task('log', function() {
@@ -51,4 +62,20 @@ gulp.task('js', function() {
 		.pipe(concat('script.js'))
 		.pipe(browserify())
 		.pipe(gulp.dest('builds/development/js'))
+});
+
+gulp.task('compass', function() {
+	/* 
+		Specify sources(sassSources)
+		Compile the source file with Compass
+		Specify destination folder ('builds...')
+	*/
+	gulp.src(sassSources)
+		.pipe(compass({
+			sass: 'components/sass',
+			image: 'builds/development/images',
+			style: 'expanded'
+		}))
+		.on('error', gutil.log)
+		.pipe(gulp.dest('builds/development/css'))
 });
