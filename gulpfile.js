5f25c5f4b604a2e6f581294006ad0aa0452aeb11
Hoonie Sun <myeonghoon18@gmail.com>
2017-06-14 10:16:44 +1000

    Combined JavaScript files using gulp-concat package

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,43 @@
-// Telling npm to bring in gulp library and assinging it to the variable
+// Tell npm to bring in gulp library and assign it to the variable
 var gulp = require('gulp'),
-	gutil = require('gulp-util');
+	/*
+		Add a new package called gulp-util
 
+		https://www.npmjs.com/package/gulp-util
+
+		Description: Utility functions for gulp plugins
+	*/
+	gutil = require('gulp-util'), 
+
+	/*
+		Add a new package called gulp-concat
+
+		https://www.npmjs.com/package/gulp-concat
+
+		Description: Concatenates files
+	*/
+	concat = require('gulp-concat');
+
+	/*
+		Create an array that includes all the javascript files 
+	*/
+var jsSources = [
+	'components/scripts/test1.js',
+	'components/scripts/test2.js'
+];
+
+// Log something during task with gulp-util package
 gulp.task('log', function() {
 	gutil.log("Workflows are awesome!")
+});
+
+gulp.task('js', function() {
+	/* 
+		Specify sources(jsSources)
+		Concatnate them into a file (script.js)
+		Specify destination folder ('builds...')
+	*/
+	gulp.src(jsSources)
+		.pipe(concat('script.js'))
+		.pipe(gulp.dest('builds/development/js'))
 });
